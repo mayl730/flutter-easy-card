265e37a3a4b05b1fd40354c9b7af24cf94578cd5
mayl730 <maylaixyz@gmail.com>
2023-08-07 13:41:40 +0900

    wip: login screen

--- lib/login/ui/screens/login_screen.dart
+++ lib/login/ui/screens/login_screen.dart
@@ -5,6 +5,35 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Login Screen');
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: SafeArea(
+        child: SingleChildScrollView(
+            child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 50.0,
+              ),
+              Image.asset(
+                'assets/images/2.0x/img_logo.png',
+                width: screenWidth * 0.6,
+                scale: 2.0,
+              ),
+              Image.asset(
+                'assets/images/2.0x/img_onboarding_card.png',
+                width: screenWidth,
+                scale: 2.0,
+              ),
+              Text('Make name card & Share it quick!'),
+            ],
+          ),
+        )),
+      ),
+    );
   }
 }
