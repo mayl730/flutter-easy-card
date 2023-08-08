1cc5b285fc1ba26fe406f6df41fde56d5a5238d7
mayl730 <maylaixyz@gmail.com>
2023-08-08 20:37:31 +0900

    add: focus node to fields and unfocus function

--- lib/my_cards/ui/screens/create_card_screen.dart
+++ lib/my_cards/ui/screens/create_card_screen.dart
@@ -1,21 +1,56 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easy_card/components/custom_action_button.dart';
 import 'package:flutter_easy_card/theme.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:go_router/go_router.dart';
 
-class CreateCardScreen extends StatelessWidget {
+class CreateCardScreen extends StatefulWidget {
   const CreateCardScreen({super.key});
 
   @override
+  State<CreateCardScreen> createState() => _CreateCardScreenState();
+}
+
+class _CreateCardScreenState extends State<CreateCardScreen> {
+  final _formKey = GlobalKey<FormBuilderState>();
+  final Map<String, FocusNode> _fieldFocusNodes = {
+    'name': FocusNode(),
+    'title': FocusNode(),
+    'company': FocusNode(),
+    'phone': FocusNode(),
+    'email': FocusNode(),
+    'website': FocusNode(),
+    'linkedin': FocusNode(),
+    'facebook': FocusNode(),
+    'twitter': FocusNode(),
+  };
+
+  @override
+  void dispose() {
+    for (var focusNode in _fieldFocusNodes.values) {
+      focusNode.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
         backgroundColor: Colors.white,
-        elevation: 0,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 15),
-          child: IconButton(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: IconButton(
               icon: const Icon(
                 Icons.close,
                 color: black,
@@ -25,25 +60,98 @@ class CreateCardScreen extends StatelessWidget {
                 GoRouter.of(context).pop();
               },
             ),
-        ),
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              'Create Card',
-              style: appTitleStyle,
-              textAlign: TextAlign.start,
-            ),
-          ],
-        ),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: sidePadding),
-          child: Column(
+          ),
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text('Create Card'),
+              Text(
+                'Create Card',
+                style: appTitleStyle,
+                textAlign: TextAlign.start,
+              ),
             ],
+          ),
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: sidePadding),
+              child: Center(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    Stack(
+                      alignment: const Alignment(1.2, 1.2),
+                      children: <Widget>[
+                        SizedBox(
+                          height: 200,
+                          width: 250,
+                          child: InkWell(
+                            onTap: () => {},
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Container(
+                                color: Colors.grey[200],
+                                child: const Icon(Icons.person),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 60,
+                          width: 60,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                          ),
+                          padding: const EdgeInsets.all(5),
+                          child: Container(
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.circle, color: easyPurple),
+                            child: IconButton(
+                              icon: const Icon(Icons.camera_alt),
+                              color: Colors.white,
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    FormBuilder(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Name',
+                            style: TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          FormBuilderTextField(
+                              name: 'name',
+                              onChanged: (val) {},
+                              onTap: () {
+                                _fieldFocusNodes['name']!.requestFocus();
+                              },
+                              focusNode: _fieldFocusNodes['name'],
+                              decoration: loginInputDecoration),
+                          const SizedBox(height: 20),
+                          CustomActionButton(
+                            label: 'Create Card',
+                            onPressed: () {
+                              context.go("/home");
+                            },
+                          ),
+                          const SizedBox(height: 30),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
