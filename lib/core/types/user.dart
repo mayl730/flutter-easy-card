83f897948fa92eb2f1a56be5242b47487358e094
mayl730 <maylaixyz@gmail.com>
2023-08-09 23:44:47 +0900

    remove: displayName field

--- lib/core/types/user.dart
+++ lib/core/types/user.dart
@@ -1,11 +1,9 @@
 class User {
-  final String displayName;
   final String email;
   final String phoneNumber;
   final String uid;
 
   User({
-    required this.displayName,
     required this.email,
     required this.phoneNumber,
     required this.uid,
