418b07001b1938a74d57da41b6a8839ec6012fcf
mayl730 <maylaixyz@gmail.com>
2023-08-09 23:41:34 +0900

    add: user and card type

---
+++ lib/core/types/card.dart
@@ -0,0 +1,29 @@
+class Card {
+  final String company;
+  final String creator;
+  final String email;
+  final String facebook;
+  final String imageUrl;
+  final bool isPrivate;
+  final String jobTitle;
+  final String linkedin;
+  final String name;
+  final String phone;
+  final String twitter;
+  final String website;
+
+  Card({
+    required this.company,
+    required this.creator,
+    required this.email,
+    required this.facebook,
+    required this.imageUrl,
+    required this.isPrivate,
+    required this.jobTitle,
+    required this.linkedin,
+    required this.name,
+    required this.phone,
+    required this.twitter,
+    required this.website,
+  });
+}
---
+++ lib/core/types/user.dart
@@ -0,0 +1,13 @@
+class User {
+  final String displayName;
+  final String email;
+  final String phoneNumber;
+  final String uid;
+
+  User({
+    required this.displayName,
+    required this.email,
+    required this.phoneNumber,
+    required this.uid,
+  });
+}
