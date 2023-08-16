1f3afb1a9bb853bb0562d62babecfc919f2a89eb
mayl730 <maylaixyz@gmail.com>
2023-08-16 11:13:30 +0900

    fix: fetchCardsByCreator method to return list of cards

--- lib/core/utils/firebase_collection_method.dart
+++ lib/core/utils/firebase_collection_method.dart
@@ -1,18 +1,45 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easy_card/core/types/card_model_with_id.dart';
 
-Future<void> fetchCardsByCreator(String creatorEmail) async {
-  CollectionReference cardCollection = FirebaseFirestore.instance.collection('cards');
-  
+Future<List<CardModelWithId>> fetchCardsByCreator(String creatorEmail) async {
+  CollectionReference cardCollection =
+      FirebaseFirestore.instance.collection('cards');
+
+  List<CardModelWithId> cardsList = [];
+
   try {
-    QuerySnapshot querySnapshot = await cardCollection.where('creator', isEqualTo: creatorEmail).get();
-    
-    for (QueryDocumentSnapshot docSnapshot in querySnapshot.docs) {
-      String docId = docSnapshot.id;
-      String cardName = docSnapshot.get('name');
-      String cardCompany = docSnapshot.get('company');
+    if (creatorEmail == "") {
+      return cardsList;
     }
-  } catch (e) {
-    print('Error fetching cards: $e');
-  }
-}
+    QuerySnapshot querySnapshot =
+        await cardCollection.where('creator', isEqualTo: creatorEmail).get();
 
+    for (QueryDocumentSnapshot docSnapshot in querySnapshot.docs) {
+      CardModelWithId card = CardModelWithId(
+        id: docSnapshot.id,
+        colorTheme: docSnapshot.get('colorTheme'),
+        company: docSnapshot.get('company'),
+        creator: creatorEmail,
+        email: docSnapshot.get('email'),
+        facebook: docSnapshot.get('facebook'),
+        imageUrl: docSnapshot.get('imageUrl'),
+        isPrivate: docSnapshot.get('isPrivate'),
+        jobTitle: docSnapshot.get('jobTitle'),
+        linkedin: docSnapshot.get('linkedin'),
+        name: docSnapshot.get('name'),
+        phone: docSnapshot.get('phone'),
+        twitter: docSnapshot.get('twitter'),
+        website: docSnapshot.get('website'),
+      );
+
+      cardsList.add(card);
+    }
+
+    print(cardsList);
+  } catch (e) {
+    debugPrint('Error fetching cards: $e');
+  }
+
+  return cardsList;
+}
