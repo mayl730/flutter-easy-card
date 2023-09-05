f01a2c4aded9586bf265facd2a4f6f5ba1c3404d
mayl730 <maylaixyz@gmail.com>
2023-09-05 10:00:11 +0900

    wip: setUser and getUser for userstore

--- lib/core/adapter/user_store.dart
+++ lib/core/adapter/user_store.dart
@@ -1,24 +1,47 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase;
 import 'package:flutter_easy_card/core/provider/user_store.dart' as provider;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserStore implements provider.UserStore {
-  static const String displayName = 'display_name';
-  static const String photoUrl = 'photo_url';
-  static const String email = 'email';
-  static const String emailVerified = 'email_verified';
-  static const String isAnonymous = 'is_anonymous';
-  static const String phoneNumber = 'phone_number';
-  static const String photoURL = 'photo_url';
-  static const String refreshToken = 'refresh_token';
-  static const String tenantId = 'tenant_id';
-  static const String uid = 'uid';
+  static const String displayNameKey = 'display_name';
+  static const String photoUrlKey = 'photo_url';
+  static const String emailKey = 'email';
+  static const String emailVerifiedKey = 'email_verified';
+  static const String isAnonymousKey = 'is_anonymous';
+  static const String phoneNumberKey = 'phone_number';
+  static const String photoURLKey = 'photo_url';
+  static const String refreshTokenKey = 'refresh_token';
+  static const String tenantIdKey = 'tenant_id';
+  static const String uidKey = 'uid';
   static const secureStorage = FlutterSecureStorage();
-  
+
   @override
-  Future<User?> getUser() {
-    // TODO: implement getUser
-    throw UnimplementedError();
+  Future<firebase.User?> getUser() async {
+    final pref = await SharedPreferences.getInstance();
+
+    final displayName = pref.getString(displayNameKey);
+    final photoUrl = pref.getString(photoUrlKey);
+    final email = pref.getString(emailKey);
+    final emailVerified = pref.getBool(emailVerifiedKey);
+    final isAnonymous = pref.getBool(isAnonymousKey);
+    final phoneNumber = pref.getString(phoneNumberKey);
+    final refreshToken = pref.getString(refreshTokenKey);
+    final tenantId = pref.getString(tenantIdKey);
+    final uid = pref.getString(uidKey);
+
+    // return User(
+    //   displayName: displayName,
+    //   photoURL: photoUrl,
+    //   email: email,
+    //   emailVerified: emailVerified!,
+    //   isAnonymous: isAnonymous!,
+    //   phoneNumber: phoneNumber,
+    //   refreshToken: refreshToken,
+    //   tenantId: tenantId,
+    //   uid: uid,
+    // );
+
   }
 
   @override
@@ -46,8 +69,18 @@ class UserStore implements provider.UserStore {
   }
 
   @override
-  Future setUser(User u) {
-    // TODO: implement setUser
-    throw UnimplementedError();
+  Future setUser(firebase.User u) async {
+    final pref = await SharedPreferences.getInstance();
+
+    await pref.setString(emailKey, u.email!);
+    await pref.setString(uidKey, u.uid);
+    await pref.setString(displayNameKey, u.displayName!);
+    await pref.setString(photoUrlKey, u.photoURL!);
+    await pref.setBool(emailVerifiedKey, u.emailVerified);
+    await pref.setBool(isAnonymousKey, u.isAnonymous);
+    await pref.setString(phoneNumberKey, u.phoneNumber!);
+    await pref.setString(refreshTokenKey, u.refreshToken!);
+    await pref.setString(tenantIdKey, u.tenantId!);
+    await pref.setString(uidKey, u.uid);  
   }
 }
