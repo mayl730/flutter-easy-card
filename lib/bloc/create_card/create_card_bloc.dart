03fa1f588c6f7683ba989f71dc99e33184511290
mayl730 <maylaixyz@gmail.com>
2023-09-09 23:42:50 +0900

    fix: get user email from userstore

--- lib/bloc/create_card/create_card_bloc.dart
+++ lib/bloc/create_card/create_card_bloc.dart
@@ -3,15 +3,18 @@ import 'dart:io';
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase;
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_easy_card/core/adapter/user_store.dart';
 import 'package:flutter_easy_card/core/types/card_model.dart';
+import 'package:flutter_easy_card/core/types/user.dart';
 
 part 'create_card_event.dart';
 part 'create_card_state.dart';
 
 class CreateCardBloc extends Bloc<CreateCardEvent, CreateCardState> {
-  CreateCardBloc() : super(CreateCardInitial()) {
+  UserStore userStore;
+  CreateCardBloc({required this.userStore}) : super(CreateCardInitial()) {
     on<CreateNewCard>((event, emit) async {
       emit(CreateCardPending());
 
@@ -28,7 +31,8 @@ class CreateCardBloc extends Bloc<CreateCardEvent, CreateCardState> {
           imageUrl = await snapshot.ref.getDownloadURL();
         }
 
-        String? creatorEmail = FirebaseAuth.instance.currentUser?.email;
+        User? user = await userStore.getUser();
+        String? creatorEmail = user?.email;
 
         CardModel updatedCardData = event.cardData.copyWith(
           imageUrl: imageUrl,
